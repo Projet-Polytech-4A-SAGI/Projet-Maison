65a7361ec7b0302b46ffb9e21cb8032172b87762
Anthonin Devas <anthonin.devas@aalto.fi>
2024-03-09 13:09:32 +0200

    Ca refonctionne

--- Express/views/script/components/cube.js
+++ Express/views/script/components/cube.js
@@ -1,13 +1,13 @@
-import { BoxBufferGeometry, Mesh, MeshStandardMaterial } from 'three';
+import * as THREE from 'three';
 
 function createCube() {
-  const geometry = new BoxBufferGeometry(2, 2, 2);
+  const geometry = new THREE.BoxGeometry(2, 2, 2);
 
   // Switch the old "basic" material to
   // a physically correct "standard" material
-  const material = new MeshStandardMaterial({ color: 'purple' });
+  const material = new THREE.MeshStandardMaterial({ color: 'purple' });
 
-  const cube = new Mesh(geometry, material);
+  const cube = new THREE.Mesh(geometry, material);
 
   cube.rotation.set(-0.5, -0.1, 0.8);
 
